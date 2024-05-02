e37169b70b3b29b1eeebe09376bd5dd689edf6be
hotsixisbest <galactagun12@gmail.com>
2024-05-02 12:16:17 +0900

    fix: songItem UI

--- src/lib/components/page/song/SongItem.svelte
+++ src/lib/components/page/song/SongItem.svelte
@@ -11,18 +11,19 @@
 
     const diffs: Difficulty[] = ["easy", "normal", "hard", "oni", "ura"];
 
-    let titleContainer: HTMLElement;
-    onMount(resizeHandle);
+    //let titleContainer: HTMLElement;
+    //onMount(resizeHandle);
     onMount(() => {
-        resizeObserver.observe(titleContainer);
-    })
+        //resizeObserver.observe(titleContainer);
+    });
 
     onDestroy(() => {
-        resizeObserver.unobserve(titleContainer);
-    })
+        //resizeObserver.unobserve(titleContainer);
+    });
 
     const [theme] = getTheme();
 
+    /*
     function resizeHandle() {
         let width =
             // @ts-expect-error
@@ -39,6 +40,7 @@
             titleContainer.children[1].display = "flex";
         }
     }
+    */
 </script>
 
 <a class="container" href={`/song/${song.songNo}`} data-theme={$theme}>
@@ -47,146 +49,161 @@
             <div
                 class="genre-item"
                 style={`background-color:${color.genre[genre]};`}
+                data-theme={$theme}
             >
-                <span>
-                    {genre}
-                </span>
+                <!--<span>{genre}</span>-->
             </div>
         {/each}
     </div>
-    <div class="title-container" bind:this={titleContainer}>
-        <div class="title">
-            {#if songLang === "jp"}
-                {song.title}
-            {:else if songLang === "ko"}
-                {song.titleKo || song.title}
-            {:else if songLang === "ako"}
-                {song.aliasKo || song.titleKo || song.title}
-            {/if}
-        </div>
-        <div class="artists">
-            <span>{song.artists.join(", ")}</span>
-        </div>
+    <div class="title-container">
+        {#if songLang === "jp"}
+            {song.title}
+        {:else if songLang === "ko"}
+            {song.titleKo || song.title}
+        {:else if songLang === "ako"}
+            {song.aliasKo || song.titleKo || song.title}
+        {/if}
     </div>
-    <div class="level-container">
-        {#each diffs as diff}
-            {#if song.courses[diff]}
-                <div
-                    class="level-item"
-                    style={`background-color:${color.difficulty[diff]};`}
-                >
-                    <span>{song.courses[diff]?.level}</span>
-                </div>
-            {:else}
-                <div class="level-item" />
-            {/if}
-        {/each}
+    <div class="third-container">
+        <div class="artists-container" data-theme={$theme}>
+            {song.artists.join(",")}
+        </div>
+        <div class="level-container">
+            {#each diffs as diff}
+                {#if song.courses[diff]}
+                    <div class="level-item" data-theme={$theme}>
+                        {song.courses[diff]?.level}
+                        <div
+                            class="level-color"
+                            style={`background-color:${color.difficulty[diff]};`}
+                            data-theme={$theme}
+                        />
+                    </div>
+                {:else}
+                    <div class="level-item" />
+                {/if}
+            {/each}
+        </div>
     </div>
 </a>
 
 <style>
     .container {
         width: 100%;
-        height: 90px;
-
-        box-sizing: border-box;
-        padding: 5px;
-
-        border-radius: 5px;
-        border: 2px solid #cf4844;
-
-        color: inherit;
-        text-decoration: none;
-
-        /*background-color: #F8F0DF;*/
 
         display: flex;
         flex-direction: column;
-        justify-content: space-evenly;
+        align-items: flex-start;
+
+        /*box-shadow: 0px 2px 2px #cf4844;*/
+        box-shadow: 0px 2px 2px #d1d1d1;
+
+        padding: 15px;
+        box-sizing: border-box;
+
+        border-radius: 5px;
+
+        color: inherit;
+        text-decoration: none;
     }
+
     .container[data-theme="dark"] {
-        border-color: black;
+        box-shadow: none;
+
+        background-color: black;
     }
-    /*
-    .container:nth-child(4n-1), .container:nth-child(4n-2){
-        background-color: rgb(225, 225, 225);
-    }
-    .container:nth-child(4n), .container:nth-child(4n-3){
-        background-color: rgb(203, 203, 203);
-    }
-    */
 
     .genre-container {
         width: 100%;
 
         display: flex;
-        justify-content: flex-start;
-        align-items: center;
-
+        flex-direction: row;
         column-gap: 5px;
     }
     .genre-item {
-        color: white;
-        font-weight: bold;
-
-        box-sizing: border-box;
-
-        padding-inline: 5px;
-
-        border-radius: 3px;
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .genre-item > span {
-        transform: translateY(-1px);
-    }
-
-    .title-container {
-        width: 100%;
-
-        display: flex;
-        flex-direction: row;
-        align-items: flex-end;
-        column-gap: 5px;
-    }
-    .title {
-        font-weight: bold;
-        font-size: 20px;
-        white-space: nowrap;
-    }
-    .artists {
-        font-size: 12px;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        transform: translateY(-2px);
-    }
-
-    .level-container {
-        width: 100%;
-
-        display: flex;
-        flex-direction: row;
-
-        column-gap: 3px;
-    }
-    .level-item {
-        width: 33px;
-        height: 20px;
+        width: 30px;
+        height: 7px;
 
         border-radius: 5px;
 
+        padding-inline: 3px;
+
+        font-size: 13px;
+
         display: flex;
         justify-content: center;
         align-items: center;
 
         color: white;
-        font-size: 14px;
+    }
+    .genre-item[data-theme="dark"]{
+        filter: brightness(85%);
+    }
+    /*
+    .genre-item > span{
+        transform: translateY(-1px);
+    }
+    */
+
+    .title-container {
+        font-weight: bold;
+
+        transform: translateY(4px);
+    }
+    .artists-container {
+        width: calc(100% - 170px);
+        height: 100%;
+        font-size: 12px;
+        color: #919191;
+
+        display:flex;
+        flex-direction: row;
+        align-items: flex-end;
+
+        transform: translateY(1px);
+    }
+    .artists-container[data-theme="dark"]{
+        color: #c9c9c9;
+    }
+    .title-container,
+    .artists-container {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    .third-container{
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+    }
+
+    .level-container {
+        width: 170px;
+
+        display: flex;
+        flex-direction: row;
+
+        column-gap: 5px;
+        font-size: 13px;
+    }
+    .level-item {
+        width: 30px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
         font-weight: bold;
     }
-    .level-item > span {
-        transform: translateY(-1px);
+    .level-color {
+        width: 100%;
+        height: 5px;
+
+        border-radius: 50vh;
+    }
+    .level-color[data-theme="dark"]{
+        filter: brightness(85%);
     }
 </style>

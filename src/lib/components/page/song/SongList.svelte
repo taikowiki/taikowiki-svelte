5768ce29b81a8c8b180630320112db3ad0b93f6b
hotsixisbest <galactagun12@gmail.com>
2024-05-02 12:19:24 +0900

    fix: third container css

--- src/lib/components/page/song/SongList.svelte
+++ src/lib/components/page/song/SongList.svelte
@@ -5,44 +5,11 @@
 
     export let songLang: SongLang;
     export let filteredSongs: (SongData & { order: number })[];
-
-    const resizeObserver = new ResizeObserver((entries) => {
-        entries.forEach((entry) => {
-            const titleContainer = entry.target as HTMLElement;
-
-            const children0 = titleContainer.children[0] as HTMLElement;
-            const children1 = titleContainer.children[1] as HTMLElement;
-
-            let width =
-                children0.offsetWidth +
-                children1.offsetWidth +
-                5;
-            if (width > titleContainer.offsetWidth) {
-                children1.style.display = "none";
-                if (
-                    children0.offsetWidth >
-                    titleContainer.offsetWidth
-                ) {
-                    children0.style.overflow = "hidden";
-                    children0.style.textOverflow = "ellipsis";
-                }
-            } else {
-                if (
-                    children0.style.textOverflow !== "ellipsis"
-                ) {
-                    children1.style.display = "block";
-                }else {
-                    children0.style.overflow = "visible";
-                    children0.style.textOverflow = "";
-                }
-            }
-        });
-    });
 </script>
 
 <div class="container">
     {#each filteredSongs as song (song.order)}
-        <SongItem {song} {songLang} {resizeObserver} />
+        <SongItem {song} {songLang}/>
     {/each}
 </div>
 

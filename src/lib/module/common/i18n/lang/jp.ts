311b7c06635fc2653ec38a8ffb9c674f73d1464d
HotsixMan <hotsixman05@gmail.com>
2024-05-21 00:25:30 +0900

    Update jp.ts

--- src/lib/module/common/i18n/lang/jp.ts
+++ src/lib/module/common/i18n/lang/jp.ts
@@ -1,10 +1,10 @@
 import type { LangFile } from "../types";
 
-const jp:LangFile = {
+const jp: LangFile = {
     layout: {
         main: {
             song: '曲',
-            doc: '文書'
+            doc: '文書',
             diffchart: '難易度表'
         },
         'dedicated diffchart': {
@@ -14,37 +14,37 @@ const jp:LangFile = {
                 dfc: '全良'
             }
         }
-    }
-},
-'/song': {
-    placeholder: 'キーワード',
-    difficulty: '難易度',
-    easy: 'かんたん',
-    normal: 'ふつう',
-    hard: 'むずかしい',
-    oni: 'おに',
-    omote: 'おに(表)',
-    ura: 'おに(裏)',
-    genre: 'ジャンル',
-    genres: {
-        pops: 'ポップス',
-        anime: 'アニメ,
-        kids: 'キッズ',
-        vocaloid: 'ボーカロイド',
-        game: 'ゲームミュージック',
-        namco: 'ナムコオリジナル',
-        variety: 'バラエティ',
-        classic: 'クラシック'
     },
-    languages:{
-        jp: '日本語',
-        ko: '韓国語',
-        ako: '韓国語(非公式)'
-    },
-    '/song/[songNo]':{
+    '/song': {
+        placeholder: 'キーワード',
+        difficulty: '難易度',
+        easy: 'かんたん',
+        normal: 'ふつう',
+        hard: 'むずかしい',
+        oni: 'おに',
+        omote: 'おに(表)',
+        ura: 'おに(裏)',
+        genre: 'ジャンル',
         genres: {
             pops: 'ポップス',
-            anime: 'アニメ,
+            anime: 'アニメ',
+            kids: 'キッズ',
+            vocaloid: 'ボーカロイド',
+            game: 'ゲームミュージック',
+            namco: 'ナムコオリジナル',
+            variety: 'バラエティ',
+            classic: 'クラシック'
+        },
+        languages: {
+            jp: '日本語',
+            ko: '韓国語',
+            ako: '韓国語(非公式)'
+        }
+    },
+    '/song/[songNo]': {
+        genres: {
+            pops: 'ポップス',
+            anime: 'アニメ',
             kids: 'キッズ',
             vocaloid: 'ボーカロイド',
             game: 'ゲームミュージック',
@@ -53,11 +53,13 @@ const jp:LangFile = {
             classic: 'クラシック'
         }
     },
-    '/diffchart':太鼓
+    '/diffchart': {
         type: {
             clear: 'クリア',
             fc: 'フルコンボ',
             dfc: '全良'
         }
-    },
+    }
+}
+
 export default jp;

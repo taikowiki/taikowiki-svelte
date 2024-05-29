9dd4e45d9a1ff9ac17b2ed06308cbaedbfcd1cf1
Hotsixman <galactagun12@gmail.com>
2024-05-29 14:00:27 +0900

    Footer에 version 추가

--- src/lib/components/layout/main/Footer.svelte
+++ src/lib/components/layout/main/Footer.svelte
@@ -1,10 +1,17 @@
+<script lang="ts">
+    export let version: string|undefined;
+</script>
+
 <div class="container">
     <div>
         © 2024 <a href="https://github.com/hotsixman">Hotsixman</a>
     </div>
     <div>
+        Taikowiki-Svelte{version ? `@${version}` : ""}
+    </div>
+    <div>
         <a href="https://github.com/taikowiki">Github</a> /
-        <a href="https://toss.me/hotsixman">Toss</a> / 
+        <a href="https://toss.me/hotsixman">Toss</a> /
         <a href="https://buymeacoffee.com/hotsixman">Buy me a coffee</a>
     </div>
 </div>
@@ -15,12 +22,12 @@
         flex-direction: column;
         align-items: center;
 
-        color:gray;
+        color: gray;
         font-size: 12px;
     }
 
-    a{
+    a {
         text-decoration: none;
-        color:inherit !important;
+        color: inherit !important;
     }
 </style>

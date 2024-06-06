29dae3739f986b54c102704c5960c1b24f5d0ccb
모메MoMe <partio2@naver.com>
2024-06-06 09:10:17 +0900

    fix: 메인화면 곡화면으로 리다이렉트

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import BannerGrid from "$lib/components/page/BannerGrid.svelte";
 
+    import { goto } from "$app/navigation";
+    goto('/song', {replaceState:true});
 </script>
 
 <BannerGrid>

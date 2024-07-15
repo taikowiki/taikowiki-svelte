b9fb3f32ef89b85944c72d6ead33be8fa60b72dc
HotsixMan <hotsixman05@gmail.com>
2024-07-15 22:37:32 +0900

    user controller 수정

--- src/lib/module/common/user/user-controller.server.ts
+++ src/lib/module/common/user/user-controller.server.ts
@@ -20,7 +20,7 @@ export default class UserController {
 
             const r = await run(`INSERT INTO \`user/data\` (\`provider\`, \`providerId\`, \`nickname\`, \`UUID\`, \`registerTime\`, \`grade\`, \`providerUserData\`) VALUES (?, ?, ?, ?, ?, ?, ?)`, [userData.provider, userData.providerId, userData.nickname, userData.UUID, userData.registerTime, userData.grade, userData.providerUserData]);
 
-            return (await run(`SELECT * FROM \`user/basic_data\` WHERE \`order\` = ?`, [r.insertId]) as UserData[])[0]
+            return (await run(`SELECT * FROM \`user/data\` WHERE \`order\` = ?`, [r.insertId]) as UserData[])[0]
         })
     }
 
@@ -57,7 +57,6 @@ export default class UserController {
     static async deleteUser(provider: string, providerId: string) {
         return await runQuery(async (run) => {
             await run(`DELETE FROM \`user/data\` WHERE \`provider\` = ? AND \`providerId\` = ?;`, [provider, providerId]);
-            await run(`DELETE FROM \`user/basic_data\` WHERE \`provider\` = ? AND \`providerId\` = ?;`, [provider, providerId]);
         })
     }
 

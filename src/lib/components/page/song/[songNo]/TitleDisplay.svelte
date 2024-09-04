8dc38d1eb0d53cba2a187735c1d32b27be8567f9
HotsixMan <hotsixman05@gmail.com>
2024-09-04 20:22:56 +0900

    feat: added youtube link to /song/[songNo] page

--- src/lib/components/page/song/[songNo]/TitleDisplay.svelte
+++ src/lib/components/page/song/[songNo]/TitleDisplay.svelte
@@ -11,13 +11,26 @@
     <div class="title">
         {title}
     </div>
-    <a class="icon-container" href={`/song/${songNo}/edit`} data-theme={$theme}>
-        <img class="icon" src="/assets/icon/pen.svg" alt="edit" />
-    </a>
+    <div class="icon-container">
+        <a
+            class="icon-anchor"
+            href={`https://www.youtube.com/results?search_query=${encodeURIComponent(`太鼓の達人 ${title}`)}`}
+            data-theme={$theme}
+        >
+            <img class="icon" src="/assets/icon/youtube.svg" alt="edit" />
+        </a>
+        <a
+            class="icon-anchor"
+            href={`/song/${songNo}/edit`}
+            data-theme={$theme}
+        >
+            <img class="icon" src="/assets/icon/pen.svg" alt="edit" />
+        </a>
+    </div>
 </h1>
 
 <style>
-    h1{
+    h1 {
         margin: 0;
     }
 
@@ -31,11 +44,19 @@
         margin-bottom: 15px;
     }
     .title {
-        flex: 1 0 auto;
+        flex: 1 1 auto;
         max-width: calc(100% - 30px);
 
         font-weight: bold;
         font-size: 35px;
+    }
+
+    .icon-container{
+        display:flex;
+        flex-direction: row;
+        margin-top: 13px;
+
+        column-gap: 4px;
     }
 
     .icon {
@@ -44,7 +65,7 @@
 
         filter: invert(100%);
     }
-    .icon-container {
+    .icon-anchor {
         width: 30px;
         height: 30px;
 
@@ -55,9 +76,8 @@
         background-color: #cf4844;
 
         border-radius: 5px;
-        margin-top: 10px;
     }
-    .icon-container[data-theme="dark"]{
+    .icon-anchor[data-theme="dark"] {
         background-color: #1c1c1c;
     }
 </style>

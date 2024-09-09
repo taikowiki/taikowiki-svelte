c4fdbbf76285067c1f6c25b476ba2987687b23c2
kongsanghyean <dodoi77@naver.com>
2024-09-09 23:06:42 +0900

    feat: added loading donder data when enter diffchart page

--- src/routes/(main)/diffchart/(dedicated diffchart)/clear/[level]/+page.server.ts
+++ src/routes/(main)/diffchart/(dedicated diffchart)/clear/[level]/+page.server.ts
@@ -1,9 +1,10 @@
 import { diffchartDBController } from "$lib/module/common/diffchart/diffchart.server";
+import { userDonderDBController } from "$lib/module/common/user/user.server";
 import { songDBController } from "$lib/module/common/song/song.server";
 import type { SongDataPickedForDiffchart } from "$lib/module/common/diffchart/types";
 import { error } from "@sveltejs/kit";
 
-export async function load({ params }) {
+export async function load({ params, locals }) {
     const level = Number(params.level);
 
     if (isNaN(level)) {
@@ -22,9 +23,15 @@ export async function load({ params }) {
         })
     })
     const songSearchResult = await songDBController.getSongsColumnsBySongNo(songNos, ["genre", "songNo", "title", "titleKo", "aliasKo"]) as SongDataPickedForDiffchart[]
+    
+    let donderDataResult = null;
+    if(locals.userData !== null) {
+        donderDataResult = await userDonderDBController.getData(locals.userData.UUID);
+    }
 
     return {
         diffChart,
-        songs: songSearchResult
+        songs: songSearchResult,
+        donderData : donderDataResult,
     }
 }

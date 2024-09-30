b2a0fb3664afbea2b81dc13b81fc24244e2ae44b
exqt <8168124+exqt@users.noreply.github.com>
2024-09-30 19:02:08 +0900

    fix: redirect if diff search param does not exist on server side

--- src/routes/(main)/song/[songNo=songNo]/+page.server.ts
+++ src/routes/(main)/song/[songNo=songNo]/+page.server.ts
@@ -1,7 +1,16 @@
 import { songDBController } from '$lib/module/common/song/song.server';
+import type { Difficulty } from '$lib/module/common/song/types.js';
+import { redirect } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load({ params, url }) {
+    const song = await songDBController.getSongBySongNo(params.songNo)
+    const diff = (url.searchParams.get('diff') ?? 'oni') as Difficulty
+
+    if (!song?.courses?.[diff]) {
+        redirect(303, `/song/${params.songNo}?diff=oni`)
+    }
+
     return {
-        song: await songDBController.getSongBySongNo(params.songNo)
+        song
     }
 }

5fffb2a3af8a2fec67f383ecbb8d5f25f5255a93
HotsixMan <hotsixman05@gmail.com>
2024-10-07 21:10:11 +0900

    .

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,23 +1,23 @@
-<div>
+<div data-type="diffchart">
     <a href="/admin/diffchart">서열표 관리</a>
 </div>
 
-<div>
+<div data-type="song">
     <a href="/admin/song">곡 관리</a>
 </div>
 
-<div>
+<div data-type="user">
     <a href="/admin/user">유저 관리</a>
 </div>
 
-<div>
+<div data-type="gamecenter">
     <a href="/admin/gamecenter">오락실 정보 관리</a>
 </div>
 
-<div>
+<div data-type="dani">
     <a href="/admin/dani">단위도장 관리</a>
 </div>
 
-<div class="image-upload">
+<div data-type="upload">
     <a href="/admin/upload">이미지 업로드</a>
 </div>

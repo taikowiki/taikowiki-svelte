b31f068f82749ade45b736e3c32bd08837ff182c
HotsixMan <hotsixman05@gmail.com>
2024-10-23 00:42:08 +0900

    .

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -205,11 +205,6 @@
         </svelte:fragment>
         <svelte:fragment slot="right">
             <User />
-            <HeaderItem
-                icon="/assets/icon/donate.svg"
-                href="/donate"
-                mobileHideSlot
-            />
         </svelte:fragment>
     </Header>
     <Main>

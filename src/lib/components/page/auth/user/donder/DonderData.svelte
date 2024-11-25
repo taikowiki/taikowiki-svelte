baed5b02d70053c907575641c4eb12313240cf12
HotsixMan <hotsixman05@gmail.com>
2024-11-25 16:46:19 +0900

    hotfix-test: luxon

--- src/lib/components/page/auth/user/donder/DonderData.svelte
+++ src/lib/components/page/auth/user/donder/DonderData.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
     import type { UserDonderData } from "$lib/module/common/user/types";
-    import dayjs from "dayjs";
-    import utc from 'dayjs/plugin/utc';
-    import timezone from 'dayjs/plugin/timezone'
     import { getTheme } from "$lib/module/layout/theme";
     import { getI18N, getLang } from "$lib/module/common/i18n/i18n";
-
-    dayjs.extend(utc);
-    dayjs.extend(timezone);
+    import { DateTime } from "luxon";
 
     export let donderData: UserDonderData;
     export let Container: ConstructorOfATypedSvelteComponent;
     export let loaded: boolean;
 
     const [theme] = getTheme();
-    const timeZone = dayjs.tz.guess();
     const lang = getLang();
-    $: i18n = getI18N('/auth/user/donder', $lang);
+    $: i18n = getI18N("/auth/user/donder", $lang);
 </script>
 
 <Container>
-    <img src={donderData.donder.myDon} alt={i18n.myDon} on:load={() => {loaded = true}} on:error={() => {loaded = true}}/>
+    <img
+        src={donderData.donder.myDon}
+        alt={i18n.myDon}
+        on:load={() => {
+            loaded = true;
+        }}
+        on:error={() => {
+            loaded = true;
+        }}
+    />
     <table data-theme={$theme}>
         <tr>
             <td class="taikonumber">
@@ -34,42 +37,43 @@
         </tr>
     </table>
     <div class="last-update">
-        {i18n.lastUpdate}: {dayjs(donderData.lastUpdate).tz(timeZone).format("YYYY-MM-DD HH:mm:ss")}
+        {i18n.lastUpdate}: {DateTime.fromJSDate(donderData.lastUpdate).toFormat("yyyy-MM-dd HH:mm:ss")}
     </div>
 </Container>
 
 <style>
-    table{
+    table {
         width: 100%;
 
         border: 1px solid black;
         border-radius: 6px;
     }
 
-    img{
+    img {
         width: 100%;
         max-width: 200px;
     }
 
-    td{
+    td {
         text-align: center;
         padding: 0;
         transform: translateY(-2px);
     }
-    tr:nth-child(1) td{
+    tr:nth-child(1) td {
         padding-bottom: 1px;
         border-bottom: 1px solid black;
     }
 
-    table[data-theme="dark"], table[data-theme="dark"] td{
+    table[data-theme="dark"],
+    table[data-theme="dark"] td {
         border-color: #818181;
     }
 
-    .taikonumber{
+    .taikonumber {
         font-size: 13px;
     }
 
-    .last-update{
+    .last-update {
         font-size: 13px;
     }
 </style>

1814cfcf2dee7d8c6ccb49b5b4737afa344dc396
kecan0406 <kecan0406@gmail.com>
2024-12-16 06:46:57 +0900

    refactor routing

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -5,21 +5,20 @@ import type { Notice } from "$lib/module/common/notice/types";
 import { runQuery } from "@yowza/db-handler";
 
 export async function load() {
-    const data: {
-        recentNotices: {
-            wiki: Omit<Notice, 'content'>[];
-            official: Omit<Notice, 'content'>[];
-        },
-        mainBanners: MainBanner[]
-    } = await runQuery(async (run) => {
-        const recentNotices = await noticeDBController.getRecentNotices.getCallback()(run);
-        const mainBanners = await bannerDBController.getMainBanner.getCallback()(run) ?? [];
+  const data: {
+    recentNotices: {
+      wiki: Omit<Notice, "content">[];
+      official: Omit<Notice, "content">[];
+    };
+    mainBanners: MainBanner[];
+  } = await runQuery(async (run) => {
+    const [recentNotices, mainBanners] = await Promise.all([
+      noticeDBController.getRecentNotices.getCallback()(run),
+      bannerDBController.getMainBanner.getCallback()(run),
+    ]);
 
-        return {
-            recentNotices,
-            mainBanners
-        }
-    })
+    return { recentNotices, mainBanners: mainBanners ?? [] };
+  });
 
-    return data;
+  return data;
 }

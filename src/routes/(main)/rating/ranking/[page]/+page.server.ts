afbfaa205d1201523a22685f5cd762c6089c5174
HotsixMan <hotsixman05@gmail.com>
2024-12-19 01:31:54 +0900

    .

--- src/routes/(main)/rating/ranking/[page]/+page.server.ts
+++ src/routes/(main)/rating/ranking/[page]/+page.server.ts
@@ -2,16 +2,14 @@ import { getTier } from "$lib/module/common/user/getTier.js";
 import { userDonderDBController } from "$lib/module/common/user/user.server";
 import { error } from "@sveltejs/kit";
 
-export async function load({ params }) {
+export async function load({ params, locals }) {
     const page = Number(params.page);
     if (isNaN(page)) {
         throw error(404);
     }
 
-    const [rankings, count] = await Promise.all([
-        userDonderDBController.getRanking(page),
-        userDonderDBController.count(),
-    ]);
+    const rankings = await userDonderDBController.getRanking(page);
+    const count = await userDonderDBController.count();
 
     const refinedRankings = rankings.map((e) => {
         const data = {
@@ -19,23 +17,25 @@ export async function load({ params }) {
             currentRating: e.currentRating,
             donder: {
                 nickname: e.donder.nickname,
-                taikoNumber: e.donder.taikoNumber,
-            } as { nickname: string | null; taikoNumber: string | null },
-            tier: getTier(e.currentRating),
-        };
-
-        if (!e.showRatingNickname) {
-            data.donder.nickname = null;
+                taikoNumber: e.donder.taikoNumber
+            } as {nickname: string | null; taikoNumber: string | null},
+            tier: getTier(e.currentRating)
         }
-        if (!e.showRatingTaikoNo) {
-            data.donder.taikoNumber = null;
+
+        if (!(locals.userData && locals.userData.grade >= 10)) {
+            if (!e.showRatingNickname) {
+                data.donder.nickname = null;
+            }
+            if (!e.showRatingTaikoNo) {
+                data.donder.taikoNumber = null;
+            }
         }
 
         return data;
-    });
+    })
 
     return {
         rankings: refinedRankings,
-        count,
-    };
+        count
+    }
 }

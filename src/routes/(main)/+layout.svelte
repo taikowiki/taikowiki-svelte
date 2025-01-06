a02927e3687bf3ba47ce969d70cb243de2ae9941
HotsixMan <hotsixman05@gmail.com>
2025-01-06 18:10:04 +0900

    .

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -197,13 +197,11 @@
         </svelte:fragment>
         <svelte:fragment slot="right">
             <User />
-            <!--
             <HeaderItem
                 icon="/assets/icon/donate.svg"
                 href="/donate"
                 mobileHideSlot
             />
-            -->
         </svelte:fragment>
     </Header>
     <Main>
--- src/routes/(main)/donate/+page.svelte
+++ src/routes/(main)/donate/+page.svelte
@@ -2,6 +2,7 @@
     <h1>
         Donate
     </h1>
+    <!--
     <a href="https://www.buymeacoffee.com/hotsixman" target="_blank"
         ><img
             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
@@ -9,6 +10,7 @@
             style="height: 60px !important;width: 217px !important;"
         /></a
     >
+    -->
     <a href="https://open.kakao.com/me/hotsixman" target="_blank"
         ><img
             src="/assets/img/kakao_donate.png"

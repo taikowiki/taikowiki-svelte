56490f432ce12074b925c8930d3f9ab0aeced547
HotsixMan <hotsixman05@gmail.com>
2025-01-07 20:13:25 +0900

    hotfix: getClientAddress

--- src/lib/module/server/hooks/logger.server.ts
+++ src/lib/module/server/hooks/logger.server.ts
@@ -1,20 +1,20 @@
-import { getClientAddress } from "$lib/module/common/util.server";
 import type { Handle } from "@sveltejs/kit";
 import { runQuery } from "@yowza/db-handler";
 
 /**
  * Records request logs.
  */
-const logger: Handle = async ({ event, resolve }) => {
-    let UUID: string | null = null;
-    if (event.locals.userData) {
-        UUID = event.locals.userData.UUID
+export const logger: Handle = async ({ event, resolve }) => {
+    try {
+        let UUID: string | null = null;
+        if (event.locals.userData) {
+            UUID = event.locals.userData.UUID
+        }
+        await runQuery(async (run) => {
+            return await run("INSERT INTO `log` (`UUID`, `ip`, `path`) VALUES (?, ?, ?)", [UUID, event.getClientAddress(), event.url.pathname + event.url.search]);
+        })
     }
-    await runQuery(async (run) => {
-        return await run("INSERT INTO `log` (`UUID`, `ip`, `path`) VALUES (?, ?, ?)", [UUID, getClientAddress(event), event.url.pathname]);
-    })
+    catch { }
 
     return await resolve(event);
 }
-
-export default logger

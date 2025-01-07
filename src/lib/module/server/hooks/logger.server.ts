c169e90a30f46be9adb87bfbe03ddac052f2e93b
HotsixMan <hotsixman05@gmail.com>
2025-01-07 20:12:51 +0900

    merge fix

--- src/lib/module/server/hooks/logger.server.ts
+++ src/lib/module/server/hooks/logger.server.ts
@@ -1,20 +1,20 @@
+import { getClientAddress } from "$lib/module/common/util.server";
 import type { Handle } from "@sveltejs/kit";
 import { runQuery } from "@yowza/db-handler";
 
 /**
  * Records request logs.
  */
-export const logger: Handle = async ({ event, resolve }) => {
-    try {
-        let UUID: string | null = null;
-        if (event.locals.userData) {
-            UUID = event.locals.userData.UUID
-        }
-        await runQuery(async (run) => {
-            return await run("INSERT INTO `log` (`UUID`, `ip`, `path`) VALUES (?, ?, ?)", [UUID, event.getClientAddress(), event.url.pathname + event.url.search]);
-        })
+const logger: Handle = async ({ event, resolve }) => {
+    let UUID: string | null = null;
+    if (event.locals.userData) {
+        UUID = event.locals.userData.UUID
     }
-    catch { }
+    await runQuery(async (run) => {
+        return await run("INSERT INTO `log` (`UUID`, `ip`, `path`) VALUES (?, ?, ?)", [UUID, getClientAddress(event), event.url.pathname]);
+    })
 
     return await resolve(event);
 }
+
+export default logger

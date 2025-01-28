8e8d604f564bec3dd2fa82844b950180b6e5f60e
HotsixMan <hotsixman05@gmail.com>
2025-01-29 00:39:47 +0900

    fix: ads

--- src/lib/components/layout/ads/MobileMainAd.svelte
+++ src/lib/components/layout/ads/MobileMainAd.svelte
@@ -5,18 +5,26 @@
     const isMobile = getIsMobile();
 </script>
 
-{#if browser}
+{#if browser && $isMobile}
     {#key $isMobile}
-        <amp-ad
-            width="100vw"
-            height="320"
-            type="adsense"
-            data-ad-client="ca-pub-1629193017650416"
-            data-ad-slot="9128318506"
-            data-auto-format="rspv"
-            data-full-width=""
-        >
-            <div overflow=""></div>
-        </amp-ad>
+        <div class="ads-container">
+            <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1629193017650416"
+                crossorigin="anonymous"
+            ></script>
+            <!-- taiko.wiki/모바일 메인 -->
+            <ins
+                class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-1629193017650416"
+                data-ad-slot="9128318506"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+            ></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     {/key}
 {/if}
--- src/lib/components/layout/ads/PcSideAd.svelte
+++ src/lib/components/layout/ads/PcSideAd.svelte
@@ -5,37 +5,30 @@
     const isMobile = getIsMobile();
 </script>
 
-{#if browser}
+{#if browser && !$isMobile}
     {#key !$isMobile}
-        <!--
-        <amp-ad
-            width="100vw"
-            height="320"
-            type="adsense"
-            data-ad-client="ca-pub-1629193017650416"
-            data-ad-slot="5810792540"
-            data-auto-format="rspv"
-            data-full-width=""
-        >
-            <div overflow=""></div>
-        </amp-ad>
--->
-        <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1629193017650416"
-            crossorigin="anonymous"
-        ></script>
-        <!-- taiko.wiki/pc사이드 -->
-        <ins
-            class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-1629193017650416"
-            data-ad-slot="5810792540"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-        ></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+        <div class="ads-container">
+            <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1629193017650416"
+                crossorigin="anonymous"
+            ></script>
+            <!-- taiko.wiki/pc사이드 -->
+            <ins
+                class="adsbygoogle"
+                style="display:inline-block;width:250px;height:250px"
+                data-ad-client="ca-pub-1629193017650416"
+                data-ad-slot="5810792540"
+            ></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     {/key}
 {/if}
+
+<style>
+    .ads-container{
+        width: 100%;
+    }
+</style>

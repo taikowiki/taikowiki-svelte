a92054f2a4d506853e7f4dc9feef322aa4ef4bd5
HotsixMan <hotsixman05@gmail.com>
2025-01-28 23:33:57 +0900

    feat: ads - mobile main

---
+++ src/lib/components/layout/ads/MobileMain.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { getIsMobile } from "$lib/module/layout/isMobile";
+
+    const isMobile = getIsMobile();
+</script>
+
+{#if browser && $isMobile}
+    <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1629193017650416"
+        crossorigin="anonymous"
+    ></script>
+    <!-- 모바일 메인 -->
+    <ins
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-1629193017650416"
+        data-ad-slot="9128318506"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+    ></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+{/if}

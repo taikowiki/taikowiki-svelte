c5c2dced44649da596957a853958e17810eade41
HotsixMan <hotsixman05@gmail.com>
2025-06-05 14:29:22 +0900

    add: hooks ns

---
+++ src/lib/module/hooks/index.ts
@@ -0,0 +1,195 @@
+import { error, redirect, type Handle } from "@sveltejs/kit";
+import { runQuery } from "@yowza/db-handler";
+import { getClientAddress } from "../common/util.server";
+import i18n from "$lib/module/common/i18n/i18n";
+import { sequence } from "@sveltejs/kit/hooks";
+import { userDBController } from "../common/user/user.server";
+
+export namespace Hooks {
+    /**
+     * 특정 Origin에서의 요청 허용
+     */
+    export function allowOrigin(allowedOrigins: string[], option?: AllowOriginOption) {
+        const handle: Handle = async ({ event, resolve }) => {
+            const origin = event.request.headers.get('Origin');
+            if (!origin) return await resolve(event);
+
+            if (allowedOrigins.includes(origin)) {
+                event.setHeaders({
+                    "Access-Control-Allow-Origin": origin,
+                    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                    "Access-Control-Allow-Headers": "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
+                });
+                if (option) {
+                    if (option?.credentials === true) {
+                        event.setHeaders({
+                            "Access-Control-Allow-Credentials": "true"
+                        })
+                    }
+                }
+            }
+
+            return await resolve(event);
+        }
+        return handle;
+    }
+
+    /**
+     * ip체크
+     */
+    export const checkIp: Handle = async ({ event, resolve }) => {
+        const banned = await runQuery(async (run) => {
+            let clientAddress: string;
+            try {
+                clientAddress = getClientAddress(event);
+                const result = await run("SELECT COUNT(*) FROM `ban/ip` WHERE `ip` = ?", [clientAddress]);
+                if (Object.values(result[0]) === undefined) {
+                    return false;
+                }
+                return Object.values(result[0])?.[0] !== 0;
+            }
+            catch {
+                return false;
+            }
+        })
+
+        if (banned) {
+            throw error(403, 'Your IP Address has been BANNED.')
+        }
+
+        return await resolve(event);
+    }
+
+    /**
+     * HTML lang 설정
+     */
+    export const dynamicHtmlLang: Handle = async ({ event, resolve }) => {
+        const urlLang = event.url.searchParams.get('lang');
+        const languages = Object.keys(i18n);
+        let lang = 'ko';
+        if (urlLang && (languages.includes(urlLang))) {
+            lang = urlLang;
+        }
+        else {
+            const cookieLang = event.cookies.get('language');
+            if (cookieLang && languages.includes(cookieLang)) {
+                lang = cookieLang;
+            }
+            else {
+                event.cookies.set('lang', 'ko', { path: '/' });
+            }
+        }
+        return await resolve(event, {
+            transformPageChunk: ({ html }) => html.replace('%lang%', lang)
+        });
+    }
+
+    /**
+     * 방문 페이지 로딩
+     */
+    export const logger: Handle = async ({ event, resolve }) => {
+        let UUID: string | null = null;
+        if (event.locals.userData) {
+            UUID = event.locals.userData.UUID
+        }
+        await runQuery(async (run) => {
+            return await run("INSERT INTO `log` (`UUID`, `ip`, `path`) VALUES (?, ?, ?)", [UUID, getClientAddress(event), event.url.pathname]);
+        })
+
+        return await resolve(event);
+    }
+
+    /**
+     * 특정 경로 요청 권한 체크
+     */
+    export function checkPermissions(options: PermissionCheckerOption[]): Handle {
+        return sequence(...options.map(option => createPermissionChecker(option.path, option.level, option.rule, option.redirectPath)))
+    }
+
+    /**
+     * DB에서 유저 데이터 가져오기
+     */
+    export const getUserData: Handle = async ({ event, resolve }) => {
+        if (event.locals.user) {
+            let userData = await userDBController.getDataByProvider(event.locals.user.provider, event.locals.user.providerId);
+            if (!userData) {
+                userData = await userDBController.createData(event.locals.user.provider, event.locals.user.providerId, event.locals.user.providerUserData ?? null)
+            }
+            event.locals.userData = userData;
+        }
+        else {
+            event.locals.userData = null;
+        }
+
+        return await resolve(event);
+    }
+
+    /**
+     * `/assets` 요청 캐시 설정
+     */
+    export const setAssetsCacheControl: Handle = async ({ event, resolve }) => {
+        if (event.url.pathname.startsWith('/assets')) {
+            event.setHeaders({
+                'Cache-Control': `max-age=${3600 * 24 * 7}`
+            });
+        }
+
+        return await resolve(event);
+    }
+
+    function createPermissionChecker(path: string, level: number, rule: 'match' | 'startsWith', redirectPath?: string): Handle {
+        return async function (input) {
+            const { locals, url } = input.event;
+
+            switch (rule) {
+                case ("match"): {
+                    if (url.pathname !== path) {
+                        return await input.resolve(input.event);
+                    }
+                    break;
+                }
+                case ("startsWith"): {
+                    if (!url.pathname.startsWith(path)) {
+                        return await input.resolve(input.event);
+                    }
+                    break;
+                }
+            }
+
+            if (!locals.user) {
+                if (!redirectPath) {
+                    throw error(403, "You have no permission to access to this page");
+                }
+
+                const param = new URLSearchParams({
+                    redirect_to: url.origin + path
+                }).toString()
+
+                throw redirect(302, url.origin + redirectPath + "?" + param)
+            }
+
+            if (!locals.userData) {
+                throw error(403, "You have no permission to access to this page");
+            }
+
+            if (locals.userData.grade < level) {
+                throw error(401, "You have no permission to access to this page");
+            }
+
+            return await input.resolve(input.event);
+        }
+    }
+}
+
+export namespace Hooks {
+    export interface AllowOriginOption {
+        credentials?: boolean
+    }
+
+    export interface PermissionCheckerOption {
+        path: string,
+        level: number,
+        rule: 'match' | 'startsWith',
+        redirectPath?: string
+    }
+}

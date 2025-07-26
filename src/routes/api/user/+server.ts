be9cd7fd7b2b76b7c0fc6a2256d294a526e2b488
hotsixman <hotsixman05@gmail.com>
2025-07-26 13:36:56 +0900

    hotfix: /api/user에서 UUID 표시

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -7,13 +7,15 @@ export async function GET(event) {
     if (locals.user && locals.userData) {
         return new Response(JSON.stringify({
             logined: true,
-            nickname: locals.userData.nickname
+            nickname: locals.userData.nickname,
+            UUID: locals.userData.UUID
         }))
     }
     else {
         return new Response(JSON.stringify({
             logined: false,
-            nickname: getClientAddress(event)
+            nickname: getClientAddress(event),
+            UUID: null
         }))
     }
 }
